8a5b5df6b7b118b4ad025b08ac88568ad247b85d
vinaykekan235 <vinaykekan235@gmail.com>
2024-05-21 15:42:25 +0530

    node api

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000; // Default to 3000 if PORT is not set in .env
+
+app.get('/hello', (req, res) => {
+    res.send('Hello');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
